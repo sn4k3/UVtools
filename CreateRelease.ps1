9d63056fcf4d8b5e1e3805ef0fc488918c60bfc8
Tiago Conceição <Tiago_caza@hotmail.com>
2020-09-14 22:23:48 +0100

    Remove Logs folder

--- CreateRelease.ps1
+++ CreateRelease.ps1
@@ -1,6 +1,8 @@
 cd $PSScriptRoot
 $version = (Get-Command UVtools.GUI\bin\Release\UVtools.Core.dll).FileVersionInfo.FileVersion
 
+Remove-Item "$PSScriptRoot\UVtools.GUI\bin\Release\Logs" -Recurse
+
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory("$PSScriptRoot\UVtools.GUI\bin\Release", "$PSScriptRoot\UVtools.GUI\bin\UVtools_v$version.zip")
 
