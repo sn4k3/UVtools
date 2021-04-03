47fb1da704495a1ffeca09c76297436c21c7f0cf
dgm3333 <dgm5555@gmail.com>
2021-04-03 09:28:12 +0100

    Use VS Community if present (Enterprise will take priority)

--- CompileWindows.bat
+++ CompileWindows.bat
@@ -2,6 +2,7 @@
 SET DIR=%~dp0
 
 REM if exist "%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" SET MSBUILD_PATH="%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" SET MSBUILD_PATH="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" SET MSBUILD_PATH="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
 
 IF [%MSBUILD_PATH%] == [] GOTO noMSBuild
