04d8fd6986200aad6cefa3e9e6a76be2a270be77
tslater2006 <tslater2006@gmail.com>
2021-10-27 08:24:54 -0500

    Create MeshFile abstraction and implement STLMeshFile

---
+++ UVtools.Core/MeshFormats/MeshFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVtools.Core.MeshFormats
+{
+    public abstract class MeshFile
+    {
+        public string FilePath;
+        public MeshFile(string filePath)
+        {
+            FilePath = filePath;
+        }
+        public abstract void Create();
+        public abstract void WriteTriangle(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 normal);
+        public abstract void Close();
+    }
+}
