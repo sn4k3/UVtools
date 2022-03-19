1d0dc64c24fbc88e3c339258c1962b7662bdd334
Tiago Conceição <Tiago_caza@hotmail.com>
2022-03-19 18:50:19 +0000

    Delete Compile.sh

--- build/Compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# This script just builds UVtools on your current system with default configuration
-# If you want to see the compilation output, go to: UVtools.WPF/bin/
-#
-cd "$(dirname "$0")"
-cd ..
-dotnet build
