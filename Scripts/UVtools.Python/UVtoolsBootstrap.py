2d6720f9cb619c61d36fda7e6bf9b40dcf94a8ee
Tiago Conceição <Tiago_caza@hotmail.com>
2022-12-09 20:37:53 +0000

    Improve imports

--- Scripts/UVtools.Python/UVtoolsBootstrap.py
+++ Scripts/UVtools.Python/UVtoolsBootstrap.py
@@ -6,8 +6,6 @@
   Everyone is permitted to copy and distribute verbatim copies
   of this license document, but changing it is not allowed.
 """
-from clr_loader import get_coreclr
-from pythonnet import set_runtime
 import sys
 import platform
 import os
