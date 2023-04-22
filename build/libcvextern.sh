fe7e644e1acb00466bb0c2045ace5c6e5c2b4f93
Tiago Conceição <Tiago_caza@hotmail.com>
2023-04-22 23:27:54 +0100

    Update libcvextern.sh

--- build/libcvextern.sh
+++ build/libcvextern.sh
@@ -166,7 +166,7 @@ echo "- Bulding"
 if [ osVariant == "macOS" ]; then
 	sed -i '' "s/-DBUILD_TIFF:BOOL=TRUE/-DBUILD_TIFF:BOOL=FALSE/g" "$directory/platforms/macos/configure" 2>/dev/null
     if [ "$build_package" == "mini" ]; then
-        sed -i "s/-DWITH_LAPACK:BOOL=OFF/-DWITH_LAPACK:BOOL=OFF -DWITH_AVFOUNDATION:BOOL=FALSE -DWITH_FFMPEG:BOOL=FALSE -DWITH_GSTREAMER:BOOL=FALSE/g" "$directory/platforms/ubuntu/22.04/cmake_configure" 2>/dev/null
+        sed -i '' "s/\$FREETYPE_OPTION/\$FREETYPE_OPTION -DWITH_AVFOUNDATION:BOOL=FALSE -DWITH_FFMPEG:BOOL=FALSE -DWITH_GSTREAMER:BOOL=FALSE/g" "$directory/platforms/ubuntu/22.04/cmake_configure" 2>/dev/null
     fi
     "$directory/platforms/macos/configure" $arch $build_package
 else # Linux
